2315755cbc950983f3dcf2bc4a877b3150e86d80
Jun Lee <li.jun4454@gmail.com>
2015-05-28 17:29:41 +0800

    add 404 page

--- application/error/error_404.php
+++ application/error/error_404.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body><h1>
-        <?php
-        echo $msg;
-        ?></h1>
-    </body>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>404</title>
+    <link href="/css/main.css" rel="stylesheet">
+</head>
+<body>
+<a id="homepage-link" href="http://www.alibudget.com/">Go to the AliBudget homepage</a>
+</body>
 </html>
